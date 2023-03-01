8d1234470e7e6012bc99d471c7379736d22f6638
Bogdan-jsx <sbogdan3755@gmail.com>
2023-03-01 19:04:47 +0200

    Minor fix

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,9 @@
 const bodyElem = document.querySelector("body");
 const loader = document.querySelector(".loader");
+const preloaderGif = document.querySelector(".loader > img")
 
 window.onload = () => {
+    preloaderGif.style.opacity = 0;
     loader.style.opacity = 0;
     setTimeout(() => loader.style.display = "none", 300);
 }
