f2b2d27b3f4ecf1fe8af9deb00e0a08e089760ad
Bogdan-jsx <sbogdan3755@gmail.com>
2023-03-01 19:22:03 +0200

    Minor fix

--- js/index.js
+++ js/index.js
@@ -2,17 +2,19 @@ const body = document.querySelector("body");
 const burgerMenuBtn = document.querySelector(".burger-menu-btn");
 const navElem = document.querySelector("nav");
 
-window.onscroll = () => {
-    if (window.innerWidth <= 1150) return;
+if (window.innerWidth > 1150) {
+    window.onscroll = () => {
+        if (window.innerWidth <= 1150) return;
 
-    const offset = window.pageYOffset;
-    const windowHeight = window.innerHeight;
-    const documentHeight = document.body.scrollHeight;
+        const offset = window.pageYOffset;
+        const windowHeight = window.innerHeight;
+        const documentHeight = document.body.scrollHeight;
 
-    const percentage = (offset / (documentHeight - windowHeight));
-    const value = -150 * percentage;
+        const percentage = (offset / (documentHeight - windowHeight));
+        const value = -150 * percentage;
 
-    body.style.backgroundPosition = `center ${value}px`;
+        body.style.backgroundPosition = `center ${value}px`;
+    } 
 }
 
 burgerMenuBtn.onclick = () => {
